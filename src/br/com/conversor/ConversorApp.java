3589a4bd4d529d0eadff2b0247fa3d4e07d65a1f
PHmore <patryckhenryck401@gmail.com>
2025-08-19 08:57:54 -0300

    Terminado versão 1.0

--- src/br/com/conversor/ConversorApp.java
+++ src/br/com/conversor/ConversorApp.java
@@ -2,9 +2,12 @@ package br.com.conversor;
 
 import br.com.conversor.principal.Principal;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class ConversorApp {
     public static void main(String[] args) {
-        System.out.println("Início projeto");
 
         Principal principal = new Principal();
         principal.exibeMenu();
--- src/br/com/conversor/principal/Principal.java
+++ src/br/com/conversor/principal/Principal.java
@@ -11,73 +11,91 @@ public class Principal {
 
     private ConverteDados converteDados = new ConverteDados();
 
-private ConsumoApi consumo = new ConsumoApi();
+    private ConsumoApi consumo = new ConsumoApi();
 
-private Scanner leitura = new Scanner(System.in);
+    private Scanner leitura = new Scanner(System.in);
 
-private  String apiKey = System.getenv("APIKEY");
+    private  String apiKey = System.getenv("APIKEY");
 
-private String ENDERECO = "https://v6.exchangerate-api.com/v6/";
+    private String ENDERECO = "https://v6.exchangerate-api.com/v6/";
 
-public void exibeMenu(){
-    var opcao = -1;
-    while (opcao != 0) {
-        var menu = """
+    public void exibeMenu(){
+        var opcao = -1;
+        while (opcao != 0) {
+            var menu = """
                     Conversor de Moedas
                     
                     1 - Real Brasileiro ==> Dólar
                     2 - Dólar ==> Real Brasileiro
-                    3 - 
-                    4 - 
-                    5 - 
-                    6 - 
+                    3 - Real Brasileiro ==> Euro
+                    4 - Euro ==> Real Brasileiro
+                    5 - Real Brasileiro ==> Iene
+                    6 - Iene ==> Real Brasileiro
                     
                     0 - Sair                                 
                     """;
 
-        System.out.println(menu);
-        opcao = leitura.nextInt();
-        leitura.nextLine();
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
+            var json = "";
 
-        switch (opcao) {
-            case 1:
-                consumo.obterDados(ENDERECO + apiKey + "/pair/USD/BRL" );
-                break;
-            case 2:
-                var json = consumo.obterDados(ENDERECO + apiKey + "/pair/USD/BRL" );
-                System.out.println(json);
-                ConversaoER conversaoER = converteDados.converterDados(json, ConversaoER.class);
-                Conversao conversao = new Conversao(conversaoER);
-                System.out.println(conversao);
+            switch (opcao) {
+                case 1:
+                    json = consumo.obterDados(ENDERECO + apiKey + "/pair/BRL/USD" );
+                    converter(json);
+                    break;
+                case 2:
+                    json = consumo.obterDados(ENDERECO + apiKey + "/pair/USD/BRL" );
+                    converter(json);
+                    break;
+                case 3:
+                    json = consumo.obterDados(ENDERECO + apiKey + "/pair/BRL/EUR" );
+                    converter(json);
+                    break;
+                case 4:
+                    json = consumo.obterDados(ENDERECO + apiKey + "/pair/EUR/BRL" );
+                    converter(json);
+                    break;
+                case 5:
+                    json = consumo.obterDados(ENDERECO + apiKey + "/pair/BRL/JPY" );
+                    converter(json);
+                    break;
+                case 6:
+                    json = consumo.obterDados(ENDERECO + apiKey + "/pair/JPY/BRL" );
+                    converter(json);
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
 
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-
-            default:
-                System.out.println("Opção inválida");
+                default:
+                    System.out.println("Opção inválida");
+            }
         }
     }
-}
 
-    public void leituraExibValor(float taxaConver,
-                                 String antes,
-                                 String depois){
-        var texto = "Digite o valor que deseja converter";
+    public void leituraExibValor(Conversao conversao){
+        System.out.println(conversao);
+        var texto = "\nDigite o valor que deseja converter: (obs: separe decimais com vírgula ,)";
+
         System.out.println(texto);
-        var valor = leitura.nextFloat();
+        var valor = leitura.nextDouble();
         leitura.nextLine();
+        var resultado = calcularTaxa(valor, conversao.getTaxaConv());
+        texto = "\n\nValor " + valor + " [" + conversao.getCodBase() + "] " + " Corresponde ao valor de ==> " + resultado + " [" + conversao.getCodAlvo() + "] \n\n";
+        System.out.println(texto);
+    }
 
-        texto = "Valor " + valor + antes + " Corresponde ao valor de ==> " + valor * taxaConver + depois;
+    public Double calcularTaxa (Double valor, Double taxa){
+        return valor * taxa;
+    }
+
+    public void converter (String json){
+        ConversaoER conversaoER = converteDados.converterDados(json, ConversaoER.class);
+        Conversao conversao = new Conversao(conversaoER);
+        //                System.out.println(conversao);
+        leituraExibValor(conversao);
     }
 
 }
